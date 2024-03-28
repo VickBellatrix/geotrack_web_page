a9fe0854ed2bfdbb8d2ee3d8b4571dbc97b7ea42
Arias3 <juan43arias@gmial.com>
2024-03-28 06:39:48 -0500

    modified:   server.js 	modified:   views/historicos.ejs 	modified:   views/historicos.php 	modified:   views/map.ejs

--- server.js
+++ server.js
@@ -67,7 +67,28 @@ server.on('connection', (socket) => {
         const fechaFormateada = `${fechaPartes[2]}-${fechaPartes[1]}-${fechaPartes[0]}`;
         latestData.fecha = fechaFormateada;
 
-        latestData.timestamp = valoresSeparados[3];
+        // Obtener la hora y los minutos de la marca de tiempo
+        const horaMinutos = valoresSeparados[3].split(':');
+        let horas = parseInt(horaMinutos[0]);
+        const minutos = horaMinutos[1];
+
+        // Convertir a formato de 24 horas si es necesario
+        const amPm = valoresSeparados[4];
+        if (amPm === 'p.' && horas !== 12) {
+            horas += 12; // Sumar 12 horas si es "p. m." y no es medianoche
+        } else if (amPm === 'a.' && horas === 12) {
+            horas = 0; // Establecer la hora a 0 si es medianoche y "a. m."
+        }
+
+        // Formatear la hora en formato de 24 horas
+        let horaFormateada;
+        if (horas === 0) {
+            horaFormateada = '00';
+        } else {
+            horaFormateada = horas.toString().padStart(2, '0'); // Asegurar que tenga dos dígitos
+        }
+
+        latestData.timestamp = `${horaFormateada}:${minutos}`;
 
         console.log(`latitud: ${latestData.lati}`);
         console.log(`longitud: ${latestData.longi}`);
@@ -79,7 +100,7 @@ server.on('connection', (socket) => {
         connection.query(sql, [latestData.lati, latestData.longi, latestData.fecha, latestData.timestamp], (error, results) => {
             if (error) console.error(error);
             else console.log("Datos insertados correctamente en la base de datos");
-            
+
         });
     });
 
@@ -133,6 +154,7 @@ app.get('/', (req, res) => {
 // Ruta para filtrar por rango de fechas
 app.get('/historicos', (req, res) => {
     res.render('historicos');
+    
 });
 
 // Ruta para filtrar por rango de fechas
--- views/historicos.php
+++ views/historicos.php
@@ -1,124 +1,113 @@
-
 <!DOCTYPE html>
-<html lang="en">
-    
+<html lang="es">
+
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" 
-    integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
-    crossorigin="anonymous">
-
- 
-    <link rel="stylesheet" href="../css/es.css">
-
-    
-
     <title>Usuarios</title>
 </head>
 
-<br>
-<div class="container is-fluid">
-
-
-<div class="col-xs-12">
-
-
-		<h2>BUSCADOR POR FECHA CON PHP</h2>
-<br>
-
-		<div>
-       
-<style>	th {
-        font-weight: bold;
-        color: white;
-    }</style>
-
-<form action="" method="GET">
-    
-                            <div class="row">
-                                
-                                <div class="col-md-4">
-                                    
-                                    <div class="form-group">
-                                        <label><b>Del Dia</b></label>
-                                        <input type="date" name="from_date" value="<?php if(isset($_GET['from_date'])){ echo $_GET['from_date']; } ?>" class="form-control">
-                                    </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="form-group">
-                                        <label><b> Hasta  el Dia</b></label>
-                                        <input type="date" name="to_date" value="<?php if(isset($_GET['to_date'])){ echo $_GET['to_date']; } ?>" class="form-control">
-                                    </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="form-group">
-                                        <label><b></b></label> <br>
-                                      <button type="submit" class="btn btn-primary">Buscar</button>
-                                    </div>
-                                </div>
+<body>
+    <div class="container is-fluid">
+        <div class="col-xs-12">
+            <h2>BUSCADOR POR FECHA CON PHP</h2>
+            <br>
+            <div>
+                <!-- Formulario de búsqueda por fecha -->
+                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="GET">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label><b>Del Día</b></label>
+                                <!-- Input para seleccionar la fecha de inicio -->
+                                <input type="date" name="from_date"
+                                    value="<?php if (isset($_GET['from_date'])) {
+                                        echo htmlspecialchars($_GET['from_date']);
+                                    } ?>"
+                                    class="form-control">
                             </div>
-                            <br>
-                        </form>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label><b>Hasta el Día</b></label>
+                                <!-- Input para seleccionar la fecha de fin -->
+                                <input type="date" name="to_date"
+                                    value="<?php if (isset($_GET['to_date'])) {
+                                        echo htmlspecialchars($_GET['to_date']);
+                                    } ?>"
+                                    class="form-control">
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label><b></b></label> <br>
+                                <!-- Botón para enviar el formulario de búsqueda -->
+                                <button type="submit" class="btn btn-primary">Buscar</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
 
+            <!-- Tabla para mostrar los resultados de la búsqueda -->
+            <table class="table table-striped" id="table_id">
+                <thead>
+                    <tr class="bg-dark">
+                        <th>Latitud</th>
+                        <th>Longitud</th>
+                        <th>Fecha</th>
+                        <th>Hora</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    // Intentar establecer conexión a la base de datos MySQL
+                    try {
+                        // Crear la conexión a la base de datos MySQL
+                        $connection = new PDO("mysql:host=db-geotrack.cj2goeeuw2ku.us-east-2.rds.amazonaws.com;dbname=db_geotrack", "admin", "17091709");
+                        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+                        // Definir las variables de fecha
+                        $from_date = isset($_GET['from_date']) ? $_GET['from_date'] : date('Y-m-d', strtotime('-1 week'));
+                        $to_date = isset($_GET['to_date']) ? $_GET['to_date'] : date('Y-m-d');
 
+                        // Consulta SQL para obtener los datos de interés
+                        $query = "SELECT latitude, longitude, fecha, hora FROM coords 
+                        WHERE (fecha BETWEEN :from_date AND :to_date) 
+                        AND (hora BETWEEN :from_hour AND :to_hour)
+                        ORDER BY fecha DESC, hora DESC LIMIT 1";
 
-                    <table class="table table-striped" id= "table_id">
-                            <thead>
-                            <tr class="bg-dark">
-                                <th>Laitud</th>
-                                <th>Longitud</th>
-                                <th>Fecha</th>
-                                <th>Hora</th>
-                                </tr>
-                            </thead>
-                            <tbody>
+                        $statement->bindParam(':from_date', $from_date);
+                        $statement->bindParam(':to_date', $to_date);
+                        $statement->execute();
+                        $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-<?php 
-                       $conexion=mysqli_connect("db-geotrack.cj2goeeuw2ku.us-east-2.rds.amazonaws.com","admin","","17091709"); 
-
-                                if(isset($_GET['from_date']) && isset($_GET['to_date']))
-                                {
-                                    $from_date = $_GET['from_date'];
-                                    $to_date = $_GET['to_date'];
-
-                                    $query = "SELECT latitud, longitud, fecha, hora FROM coords
-                                    LEFT JOIN permisos ON user.rol = permisos.id WHERE fecha BETWEEN '$from_date' AND '$to_date' ";
-                                    $query_run = mysqli_query($conexion, $query);
-
-                                    if(mysqli_num_rows($query_run) > 0)
-                                    {
-                                        foreach($query_run as $fila)
-                                        {
-                                            ?>
-                                            <tr>
-                                            <td><?php echo $fila['latitud']; ?></td>
-                                            <td><?php echo $fila['longitud']; ?></td>
-                                            <td><?php echo $fila['fecha']; ?></td>
-                                            <td><?php echo $fila['hora']; ?></td>
-                                            <td><img src="../imgs/<?php echo $fila['imagen']; ?>" 
-                                            onerror=this.src="../imgs/noimage.png" width="50" heigth="70"></td>
-                                            </tr>
-                                            <?php
-                                        }
-                                    }
-                                    else
-                                    {
-                                        ?>
-                                      
-                                         <tr>
-                                         <td><?php  echo "No se encontraron resultados"; ?></td>
-                                  <?php
-                                    }
-                                }
-                            ?>
-		</div>
-
-
-
-
+                        // Verificar si se encontraron resultados
+                        if ($rows) {
+                            // Mostrar los datos en la tabla
+                            foreach ($rows as $row) {
+                                echo "<tr>";
+                                echo "<td>" . htmlspecialchars($row['latitude']) . "</td>";
+                                echo "<td>" . htmlspecialchars($row['longitude']) . "</td>";
+                                echo "<td>" . htmlspecialchars($row['fecha']) . "</td>";
+                                echo "<td>" . htmlspecialchars($row['hora']) . "</td>";
+                                echo "</tr>";
+                            }
+                        } else {
+                            echo "<tr><td colspan='4'>No se encontraron resultados</td></tr>";
+                        }
+                    } catch (PDOException $e) {
+                        // Mensaje de error en la consola del navegador
+                        echo "<script>console.error('Error al conectar a la base de datos MySQL: " . $e->getMessage() . "');</script>";
+                    } finally {
+                        // Cerrar la conexión a la base de datos
+                        $connection = null;
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</body>
 
 </html>

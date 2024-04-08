642c587fde375367c6345184b7b742d6f72d5c2f
mjnova <munozmateo16@hotmail.com>
2024-04-08 01:56:16 -0500

    area

--- server.js
+++ server.js
@@ -5,18 +5,8 @@ const path = require('path');
 const historicosview = require('./historicosview');
 require('dotenv').config();
 
-const dbHost = process.env.host;
-const dbUser = process.env.user;
-const dbPassword = process.env.password;
-const dbName = process.env.database;
-const dBMaster = process.env.MAESTRO;
 
-
-//Variables de entorno 
-console.log(dbHost, dbUser, dbPassword, dbName, dBMaster);
-
-require('dotenv').config(); // Cargar variables de entorno desde el archivo .env
-
+//============================================================================
 // Inicializar latestData para almacenar los últimos datos recibidos del sniffer
 let latestData = {
     lati: 0,
@@ -27,13 +17,19 @@ let latestData = {
 
 // Configurar la conexión a la base de datos
 const connection = mysql.createConnection({
-    host: process.env.host,
-    user: process.env.user,
-    password: process.env.password,
-    database: process.env.database,
-    isMaster: process.env.MAESTRO,
 
+    //Sara BD
+    //host: 'db-geotrack.cj2goeeuw2ku.us-east-2.rds.amazonaws.com',
+    //user: 'admin',
+    //password: '17091709',
+
+    host: 'database-1.chyoicow6j06.us-east-2.rds.amazonaws.com',
+    user: 'admin',
+    password: 'adastra2',
+
+    database: 'geotrack',
 });
+
 
 // Conección a la base de datos
 connection.connect(function (err) {
@@ -44,17 +40,57 @@ connection.connect(function (err) {
     }
 });
 
+//============================================================================
+// const dbHost = process.env.host;
+// const dbUser = process.env.user;
+// const dbPassword = process.env.password;
+// const dbName = process.env.database;
+// const dBMaster = process.env.MAESTRO;
 
-// Definir isMaster después de la conexión a la base de datos
-const isMaster = process.env.MAESTRO === 'true';
 
-if (isMaster) {
-    // Código para escribir en la base de datos
-    console.log('Esta instancia es el maestro, realizando operaciones de escritura en la base de datos...');
-} else {
-    // Si esta instancia no es el maestro, abstenerse de realizar operaciones de escritura
-    console.log('Esta instancia no es el maestro, no se realizan operaciones de escritura en la base de datos.');
-}
+// //Variables de entorno 
+// console.log(dbHost, dbUser, dbPassword, dbName, dBMaster);
+
+// require('dotenv').config(); // Cargar variables de entorno desde el archivo .env
+
+// // Inicializar latestData para almacenar los últimos datos recibidos del sniffer
+// let latestData = {
+//     lati: 0,
+//     longi: 0,
+//     fecha: 0,
+//     timestamp: 0
+// };
+
+// // Configurar la conexión a la base de datos
+// const connection = mysql.createConnection({
+//     host: process.env.host,
+//     user: process.env.user,
+//     password: process.env.password,
+//     database: process.env.database,
+//     isMaster: process.env.MAESTRO,
+
+// });
+
+// Conección a la base de datos
+// connection.connect(function (err) {
+//     if (err) {
+//         throw err;
+//     } else {
+//         console.log("¡Conexión exitosa con la base de datos!")
+//     }
+// });
+
+
+// // Definir isMaster después de la conexión a la base de datos
+// const isMaster = process.env.MAESTRO === 'true';
+
+// if (isMaster) {
+//     // Código para escribir en la base de datos
+//     console.log('Esta instancia es el maestro, realizando operaciones de escritura en la base de datos...');
+// } else {
+//     // Si esta instancia no es el maestro, abstenerse de realizar operaciones de escritura
+//     console.log('Esta instancia no es el maestro, no se realizan operaciones de escritura en la base de datos.');
+// }
 
 const dgram = require('dgram');
 
@@ -140,6 +176,11 @@ app.get('/', (req, res) => {
 // Ruta para filtrar por rango de fechas
 app.get('/historicos', (req, res) => {
     res.render('historicos');
+
+});
+
+app.get('/historicosMP', (req, res) => {
+    res.render('historicosMP');
 
 });
 

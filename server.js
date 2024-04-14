2e4481b0d83d45a64e4a097c6021ec1beff4740d
mjnova <munozmateo16@hotmail.com>
2024-04-13 23:58:02 -0500

    login para BD2

--- server.js
+++ server.js
@@ -7,6 +7,46 @@ require('dotenv').config();
 
 const mode = 1;
 
+ // Inicializar latestData para almacenar los últimos datos recibidos del sniffer
+    let latestData = {
+        lati: 0,
+        longi: 0,
+        fecha: 0,
+        timestamp: 0
+    };
+
+    // Configurar la conexión a la base de datos
+    const connection = mysql.createConnection({
+
+        //Sara BD
+        //host: 'db-geotrack.cj2goeeuw2ku.us-east-2.rds.amazonaws.com',
+        //user: 'admin',
+        //password: '17091709',
+
+        host: 'database-1.chyoicow6j06.us-east-2.rds.amazonaws.com',
+        user: 'admin',
+        password: 'adastra2',
+
+        database: 'geotrack',
+    });
+
+
+    // Conección a la base de datos
+    connection.connect(function (err) {
+        if (err) {
+            throw err;
+        } else {
+            console.log("¡Conexión exitosa con la base de datos!")
+        }
+    });
+
+
+    const dbHost = process.env.host;
+    const dbUser = process.env.user;
+    const dbPassword = process.env.password;
+    const dbName = process.env.database;
+    const dBMaster = process.env.MAESTRO;
+
 // Definir isMaster después de la conexión a la base de datos
 const isMaster = process.env.MAESTRO === 'true';
 
@@ -51,7 +91,7 @@ server.on('message', (msg, rinfo) => {
     console.log(`Usuario: ${latestData.usuario}`);
 
     // Inserción de los datos en la base de datos
-  //  if (isMaster) {
+    if (isMaster) {
         const sql = `INSERT INTO coords (latitud, longitud, fecha, hora, usuario) VALUES (?, ?, ?, ?, ?)`;
         connection.query(sql, [latestData.lati, latestData.longi, latestData.fecha, latestData.timestamp, latestData.usuario], (error, results) => {
             if (error) console.error(error);
@@ -59,7 +99,7 @@ server.on('message', (msg, rinfo) => {
 
         });
     }
-//});
+});
 
 // Manejar errores
 server.on('error', (err) => {

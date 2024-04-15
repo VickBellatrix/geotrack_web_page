000ba80a17c8848194e340fcf4d6077da74468cb
mjnova <munozmateo16@hotmail.com>
2024-04-14 21:22:09 -0500

    server para instancias

--- server.js
+++ server.js
@@ -7,45 +7,45 @@ require('dotenv').config();
 
 const mode = 1;
 
- // Inicializar latestData para almacenar los últimos datos recibidos del sniffer
-    let latestData = {
-        lati: 0,
-        longi: 0,
-        fecha: 0,
-        timestamp: 0
-    };
-
-    // Configurar la conexión a la base de datos
-    const connection = mysql.createConnection({
-
-        //Sara BD
-        //host: 'db-geotrack.cj2goeeuw2ku.us-east-2.rds.amazonaws.com',
-        //user: 'admin',
-        //password: '17091709',
-
-        host: 'database-1.chyoicow6j06.us-east-2.rds.amazonaws.com',
-        user: 'admin',
-        password: 'adastra2',
-
-        database: 'geotrack',
-    });
+const dbHost = process.env.host;
+const dbUser = process.env.user;
+const dbPassword = process.env.password;
+const dbName = process.env.database;
+const dBMaster = process.env.MAESTRO;
 
 
-    // Conección a la base de datos
-    connection.connect(function (err) {
-        if (err) {
-            throw err;
-        } else {
-            console.log("¡Conexión exitosa con la base de datos!")
-        }
-    });
+//Variables de entorno 
+console.log(dbHost, dbUser, dbPassword, dbName, dBMaster);
 
+require('dotenv').config(); // Cargar variables de entorno desde el archivo .env
 
-    const dbHost = process.env.host;
-    const dbUser = process.env.user;
-    const dbPassword = process.env.password;
-    const dbName = process.env.database;
-    const dBMaster = process.env.MAESTRO;
+// Inicializar latestData para almacenar los últimos datos recibidos del sniffer
+let latestData = {
+    lati: 0,
+    longi: 0,
+    fecha: 0,
+    timestamp: 0
+};
+
+// Configurar la conexión a la base de datos
+const connection = mysql.createConnection({
+    host: process.env.host,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database,
+    isMaster: process.env.MAESTRO,
+
+});
+
+//Conección a la base de datos
+connection.connect(function (err) {
+    if (err) {
+        throw err;
+    } else {
+        console.log("¡Conexión exitosa con la base de datos!")
+    }
+});
+
 
 // Definir isMaster después de la conexión a la base de datos
 const isMaster = process.env.MAESTRO === 'true';
